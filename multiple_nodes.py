77b6fece38fde2894698cb38cc9184702721bff1
EquineNaveen <snaveengowda13063@gmail.com>
2025-06-17 15:17:31 +0530

    done with third excercise

---
+++ multiple_nodes.py
@@ -0,0 +1,38 @@
+from typing import TypedDict,List
+from langgraph.graph import StateGraph
+
+class AgentState(TypedDict):
+    name: str
+    age: int
+    skills: List[str]
+    result:str
+
+def first_node(state: AgentState) -> AgentState:
+    state['result'] =f"{state['name']} ,welcome to the system"
+    return state
+
+def second_node(state: AgentState) -> AgentState:
+    state['result'] = f"{state['result']}, you are {state['age']} years old!"
+    return state
+
+def third_node(state:AgentState) -> AgentState:
+    """This node will list the user's skills in a formatted string"""
+    state["result"] = state["result"] + f" You have skills in: {', '.join(state['skills'])}"
+
+    return state
+
+graph=StateGraph(AgentState)
+graph.add_node("first_node", first_node)
+graph.add_node("second_node", second_node)
+graph.add_node("third_node", third_node)
+
+graph.set_entry_point("first_node")
+graph.add_edge("first_node", "second_node")
+graph.add_edge("second_node", "third_node") 
+graph.set_finish_point("third_node")
+
+app=graph.compile()
+
+result = app.invoke({"name": "Linda", "age": 31, "skills":["Python", "Machine Learning", "LangGraph"]})
+
+print(result['result'])  # Output: Linda ,welcome to the system, you are 31 years old! You have skills in: Python, Machine Learning, LangGraph
